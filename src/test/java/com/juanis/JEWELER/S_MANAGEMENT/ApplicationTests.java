cc490f9c834716e6909f93a03b47731ad636ce32
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2024-09-03 19:07:09 -0500

    initial

---
+++ src/test/java/com/juanis/JEWELER/S_MANAGEMENT/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juanis.JEWELER.S_MANAGEMENT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
