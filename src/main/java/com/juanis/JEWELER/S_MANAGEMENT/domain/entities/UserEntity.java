68df8d66a114ca8911f11a65ce3a89535aa45f34
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2024-09-03 19:26:56 -0500

    User-Entity

--- src/main/java/com/juanis/JEWELER/S_MANAGEMENT/domain/entities/UserEntity.java
+++ src/main/java/com/juanis/JEWELER/S_MANAGEMENT/domain/entities/UserEntity.java
@@ -1,4 +1,29 @@
 package com.juanis.JEWELER.S_MANAGEMENT.domain.entities;
 
+import com.juanis.JEWELER.S_MANAGEMENT.utils.enums.Role;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 15)
+    private String name;
+
+    @Column(nullable = false, length = 15)
+    private String email;
+
+    @Column(nullable = false, length = 15)
+    private String password;
+
+    @Column(nullable = false, length = 15)
+    private Role role;
 }
