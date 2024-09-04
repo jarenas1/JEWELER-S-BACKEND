abc346407945ce1efc1417d60bee6708b9de8c38
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2024-09-03 19:32:57 -0500

    Review-entity

--- src/main/java/com/juanis/JEWELER/S_MANAGEMENT/domain/entities/Review.java
+++ src/main/java/com/juanis/JEWELER/S_MANAGEMENT/domain/entities/Review.java
@@ -1,4 +1,31 @@
 package com.juanis.JEWELER.S_MANAGEMENT.domain.entities;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long rating;
+
+    private String description;
+
+    private UserEntity user;
+
+    private  Product product;
+
+    private Timestamp cratedAt;
 }
